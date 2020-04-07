71692d23e67148c6539a5e8e43a343de43ff4292
Marina Tavares <marinastavares6@gmail.com>
2020-04-07 08:58:07 -0300

    new images

--- index.js
+++ index.js
@@ -15,5 +15,5 @@ app.get('/', (req, res) => {
 
 app.listen(port, () => {
   console.log(`Server running on port: ${port}`);
-  console.log('Press CTRL + C to quit');
+  console.log('Press CTRL +s C to quit');
 })
